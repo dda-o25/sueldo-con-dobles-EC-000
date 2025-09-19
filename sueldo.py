82676f4e9d85e0ec2fc35dc71f535f1e668d89de
EC-000 <eduardoc.castillo9814@gmail.com>
2025-09-18 22:48:40 -0600

    v4
    cambie los int por float
    saque las variables para que se impriman aparte las horas extras, el pago extra y el sueldo total

--- sueldo.py
+++ sueldo.py
@@ -3,18 +3,22 @@ El propósito de este código es dado un número de horas y de tarifa por hora, 
 asi como tambien las horas extras calculadas despues de la hora numero 48 son cobradas al doble
 """
 
-
 # Entradas
-horas_trabajadas = int(input())
-tarifa_hora = int(input())
+horas_trabajadas = float(input())
+tarifa_hora = float(input())
 
 # Proceso
 if horas_trabajadas > 48:
     horas_normales = 48
     horas_extra = horas_trabajadas - 48
-    sueldo = (horas_normales * tarifa_hora) + (horas_extra * tarifa_hora * 2)
+    pago_extra = horas_extra * tarifa_hora * 2
+    sueldo_total = (horas_normales * tarifa_hora) + pago_extra
 else:
-    sueldo = horas_trabajadas * tarifa_hora
+    horas_extra = 0
+    pago_extra = 0
+    sueldo_total = horas_trabajadas * tarifa_hora
 
 # Salidas
-print(sueldo)
+print(int(horas_extra))
+print(int(pago_extra))
+print(int(sueldo_total))
