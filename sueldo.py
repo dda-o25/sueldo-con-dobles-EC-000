c428c4eb235695d0fafdc2acbfdfc1be902a26c5
EC-000 <eduardoc.castillo9814@gmail.com>
2025-09-18 22:41:07 -0600

    v1
    Primer prueba del codigo, para revisar el autocheking

--- sueldo.py
+++ sueldo.py
@@ -1,16 +1,20 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+El propósito de este código es dado un número de horas y de tarifa por hora, calcular el sueldo de x persona
+asi como tambien las horas extras calculadas despues de la hora numero 48 son cobradas al doble
 """
 
 
-# Declaraciones
-CONSTANTE = valor
-
 # Entradas
-entrada = input()
+horas_trabajadas = int(input())
+tarifa_hora = int(input())
 
 # Proceso
-
+if horas_trabajadas > 48:
+    horas_normales = 48
+    horas_extra = horas_trabajadas - 48
+    sueldo = (horas_normales * tarifa_hora) + (horas_extra * tarifa_hora * 2)
+else:
+    sueldo = horas_trabajadas * tarifa_hora
 
 # Salidas
-print(salida)
+print(sueldo)
